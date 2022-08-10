2ee999f2d237ef32e79c2579f507fc45f264b9fe
hyb <13407937436@163.com>
2022-08-10 18:09:08 +0800

    删除多余代码

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -18,78 +18,7 @@ export class IndexComponent implements OnInit {
   dark = false;
 
   menuList : Array<Menu>;
-  // menuList = [
-  //   {
-  //     level: 1,
-  //     title: 'Mail Group',
-  //     icon: 'mail',
-  //     open: true,
-  //     selected: false,
-  //     disabled: false,
-  //     children: [
-  //       {
-  //         level: 2,
-  //         title: 'Group 1',
-  //         icon: 'bars',
-  //         open: false,
-  //         selected: false,
-  //         disabled: false,
-  //         children: [
-  //           {
-  //             level: 3,
-  //             title: 'Option 1',
-  //             selected: false,
-  //             disabled: false
-  //           },
-  //           {
-  //             level: 3,
-  //             title: 'Option 2',
-  //             selected: false,
-  //             disabled: true
-  //           }
-  //         ]
-  //       },
-  //       {
-  //         level: 2,
-  //         title: 'Group 2',
-  //         icon: 'bars',
-  //         selected: true,
-  //         disabled: false
-  //       },
-  //       {
-  //         level: 2,
-  //         title: 'Group 3',
-  //         icon: 'bars',
-  //         selected: false,
-  //         disabled: false
-  //       }
-  //     ]
-  //   },
-  //   {
-  //     level: 1,
-  //     title: 'Team Group',
-  //     icon: 'team',
-  //     open: false,
-  //     selected: false,
-  //     disabled: false,
-  //     children: [
-  //       {
-  //         level: 2,
-  //         title: 'User 1',
-  //         icon: 'user',
-  //         selected: false,
-  //         disabled: false
-  //       },
-  //       {
-  //         level: 2,
-  //         title: 'User 2',
-  //         icon: 'user',
-  //         selected: false,
-  //         disabled: false
-  //       }
-  //     ]
-  //   }
-  // ];
+ 
   constructor(private indexSevice: IndexService, private nzMessageService: NzMessageService, private router: Router) { }
 
   ngOnInit(): void {
@@ -107,9 +36,7 @@ export class IndexComponent implements OnInit {
 
   getMenu() {
     this.indexSevice.getMenu().subscribe(res => {
-      //console.log('获取菜单：'+JSON.stringify(res.data));
       this.menuList = res.data;
-      console.log('获取菜单：' + JSON.stringify(this.menuList));
     });
   }
 
