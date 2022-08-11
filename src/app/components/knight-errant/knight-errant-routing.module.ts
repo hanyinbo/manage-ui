f8719f69f4c67ba3b08da97eea6938a24c194b4a
hyb <13407937436@163.com>
2022-08-11 16:26:41 +0800

    修改全局样式

--- src/app/components/knight-errant/knight-errant-routing.module.ts
+++ src/app/components/knight-errant/knight-errant-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
 import { KnightErrantListComponent } from './knight-errant-list/knight-errant-list.component';
 import { KnightErrantSettingsComponent } from './knight-errant-settings/knight-errant-settings.component';
 const routes: Routes = [
